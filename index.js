4c38334cacb2d762c84ae1ac659485a0d531c1e4
Josiah Bills <Josiah@adoniram.net>
2015-09-28 09:37:37 -0400

    Hover colors.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function enter_handler() {
-	$(".word").css("opacity", "0");
-	$("." + this.classList[1]).css("opacity", "1");
+	leave_handler();
+	$("." + this.classList[1]).css("opacity", "1").css("color", "#5d5d5d");
 }
 
 function leave_handler() {
-	$(this).css("visibility", "hidden").off("mouseleave");
+	$(".word").css("opacity", "0").css("color", "");
+	$(".letter").css("color", "");
 }
 
 $(function() {
 	$(".letter").mouseenter(enter_handler);
 	$(".word").mouseenter(enter_handler);
 
+	$(".letter").mouseleave(leave_handler);
+	$(".word").mouseleave(leave_handler);
 });
